2abab4411359876f838b639c4d33233b6321726e
Murli913 <141387276+Murli913@users.noreply.github.com>
2024-04-10 20:11:48 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ sudo apt update -y
 sudo apt install temurin-17-jdk -y
 /usr/bin/java --version
 
-sqp_45e8752e2f0636f18b67629a2243b9cc5a4e0c05
+sqp_317a0f78c6440e32502e39971555f78ba37ac0e3
 # Install Trivy
 sudo apt-get install wget apt-transport-https gnupg lsb-release -y
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
